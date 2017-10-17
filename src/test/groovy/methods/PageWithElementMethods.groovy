146179ddb2064535deb899284ca9da48bd6e9532
Eduard Fugarolas <edufuga@gmail.com>
2017-10-17 14:59:30 +0200

    Example of returning a list of Strings. Waiting inside the method body. Collect instead of spread-dot operator for type checking.

--- src/test/groovy/methods/PageWithElementMethods.groovy
+++ src/test/groovy/methods/PageWithElementMethods.groovy
@@ -5,6 +5,7 @@ import geb.Browser
 import geb.Page
 import geb.navigator.Navigator
 import groovy.transform.TypeChecked
+import groovy.transform.TypeCheckingMode
 
 @TypeChecked
 class PageWithElementMethods extends TypedPage {
@@ -23,7 +24,19 @@ class PageWithElementMethods extends TypedPage {
 	 * Searches the given word.
 	 */
 	void search(String word) {
+		println "Search word '$word'."
 		searchBox.value(word)
+		println "Results: "
+		words?.each {
+			println it
+		}
+	}
+
+	/*
+	 * Returns the list of words matched by the dictionary.
+	 */
+	List<String> getWords() {
+		return getDefinition({$("#typeahead-menu").$("li")}, true).collect { Navigator n -> n.text() }
 	}
 
 }
