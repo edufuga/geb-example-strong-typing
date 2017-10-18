b815c173542e84f3f5a28885aefe46f584d5bae6
Eduard Fugarolas <edufuga@gmail.com>
2017-10-18 18:41:55 +0200

    Documentation.

--- src/test/groovy/methods/PageWithElementMethods.groovy
+++ src/test/groovy/methods/PageWithElementMethods.groovy
@@ -13,8 +13,30 @@ class PageWithElementMethods extends TypedPage {
 
 	static url = "http://de.pons.com/"
 
+	/**
+	 * Final parameterless Closure.
+	 */
 	private final Closure<Navigator> _searchBox = {$("input#q")}
 
+	/**
+	 * Search box.
+	 * 
+	 * <p>
+	 * The Navigator (search box) comes from a final parameterless
+	 * Closure that is cached and waited for.
+	 * 
+	 * <p>
+	 * The caching just works when the closure {@link _searchBox}
+	 * is final. It would for example not work if the code of the
+	 * Closure, i.e. <code>{$("input#q")}</code>, was written
+	 * directly as an argument of the {@link #getDefinition} method,
+	 * i.e. <code>getDefinition({$("input#q")}, true, true)</code>.
+	 * The reason is: Every time the method is called, a new Closure
+	 * is created; although it has the same code, its hashcode is
+	 * different at every method call.
+	 * 
+	 * @return Search box.
+	 */
 	Navigator getSearchBox() {
 		getDefinition(_searchBox, true, true)
 	}
@@ -27,15 +49,50 @@ class PageWithElementMethods extends TypedPage {
 		$(".nav-secondary")
 	}
 
+	/**
+	 * Parameterized Closure.
+	 * 
+	 * <p>
+	 * Memoized for caching closure calls with its parameter and return value.
+	 */
 	private final Closure<Navigator> _menu = {int idx -> bar.$("li", idx)}.memoize()
 
-	private final Closure<Navigator> waitedMenu = {int idx -> this.&getDefinition({_menu(idx)}, true, true)}
+	/**
+	 * Parameterized Closure.
+	 * 
+	 * <p>
+	 * A call to the Closure {@link #_menu} with the parameter is
+	 * written inside of <em>another</em> Closure; that Closure is
+	 * parameterless.
+	 * 
+	 * <p>
+	 * The parameterless Closure is passed to {@link #getDefinition}
+	 * for enabling caching
+	 * (the Closure {@link _menu} has to be <strong>final</strong>)
+	 * and waiting.
+	 * 
+	 * <p>
+	 * The method {@link #getDefinition} is used as a method reference
+	 * in order to define the resulting Closure.
+	 */
+	final Closure<Navigator> waitedMenu = {int idx -> this.&getDefinition({_menu(idx)}, true, true)}
 
+	/**
+	 * Parameterized method.
+	 * 
+	 * <p>
+	 * A call to the Closure {@link #_menu} with the parameter is
+	 * written inside of <em>another</em> Closure; that Closure is
+	 * parameterless.
+	 * 
+	 * @param idx Index
+	 * @return Menu at index
+	 */
 	Navigator menu(int idx) {
 		getDefinition({_menu(idx)}, true, true)
 	}
 
-	/*
+	/**
 	 * Searches the given word.
 	 */
 	void search(String word) {
@@ -47,8 +104,15 @@ class PageWithElementMethods extends TypedPage {
 		}
 	}
 
-	/*
+	/**
 	 * Returns the list of words matched by the dictionary.
+	 * 
+	 * <p>
+	 * The spread dot operator can't be used with
+	 * <code>@TypeChecked</code>; <code>collect</code>
+	 * is used instead with a typed parameter.
+	 * 
+	 * @return List of words.
 	 */
 	List<String> getWords() {
 		return getDefinition({$("#typeahead-menu").$("li")}, true).collect { Navigator n -> n.text() }
