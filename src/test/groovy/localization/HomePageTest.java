01346bbbb0c0ef7d52902b830f29ba3bb4072350
Eduard Fugarolas <edufuga@gmail.com>
2017-10-20 18:26:30 +0200

    JUnit/Java power.

---
+++ src/test/groovy/localization/HomePageTest.java
@@ -0,0 +1,22 @@
+package localization;
+
+import geb.junit4.GebReportingTest;
+	import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class HomePageTest extends GebReportingTest {
+
+    @Test
+    public void simpleChangeFromSpanishToGerman() {
+    	// when
+		HomePageSpanish page = browser.<HomePageSpanish> to(HomePageSpanish.class);
+		// page.run(new HomePageGermanToSpanishGroovyAction()); // Java power.
+		page.run(new HomePageSpanishToGermanAction());
+
+		// then
+		browser.at(HomePageGerman.class);
+
+    }
+}
