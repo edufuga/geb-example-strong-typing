48ea694b2b1bea17c4be2489602ceaad0e007798
Eduard Fugarolas <edufuga@gmail.com>
2017-10-22 20:07:36 +0200

    Original HomePageTest recovered. BDD version in separate class.

--- src/test/groovy/localization/HomePageTest.java
+++ src/test/groovy/localization/HomePageTest.java
@@ -1,53 +1,28 @@
 package localization;
 
 import geb.junit4.GebReportingTest;
-
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-import behaviour.Behaviour;
-
-import static behaviour.Behaviour.given;
 
 @RunWith(JUnit4.class)
 public class HomePageTest extends GebReportingTest {
 
     @Test
     public void simpleChangeFromSpanishToGerman() {
-    	assertTrue(new Behaviour(browser).given(browser -> browser.to(HomePageSpanish.class))
-    	.when(page -> page.run(new HomePageSpanishToGermanAction()))
-    	.then(HomePageGerman::verifyAt));
+    	// when
+		HomePageSpanish page = browser.<HomePageSpanish> to(HomePageSpanish.class);
+		// page.run(new HomePageGermanToSpanishGroovyAction()); // Java power.
+		page.run(new HomePageSpanishToGermanAction());
+
+		// then
+		browser.at(HomePageGerman.class);
     }
 
     @Test
     public void thereAndBackAgain() {
-    	assertTrue(given(() ->	browser.to(HomePageSpanish.class))
-    	.when(spanish -> spanish.run(new HomePageSpanishToGermanAction()))
-    	.then(german -> german.run(new HomePageGermanToSpanishJavaAction()).verifyAt()));
-    }
-
-    @Test
-    public void wordList() {
-		assertTrue("The list of words is empty",
-			given(() -> browser.to(HomePageSpanish.class))
-			.when(spanishPage -> {
-				spanishPage.search("asac");
-				return spanishPage;
-			})
-			.when(spanishPage -> {
-				spanishPage.search("casa");
-				return spanishPage;
-			})
-			.then(spanishPage -> !spanishPage.getWords().isEmpty()));
-    }
-
-    @Test
-    public void spanishTitle() {
-		assertTrue("The spanish title is empty",
-			given(() -> browser.to(HomePageSpanish.class))
-			.then(page -> !page.getMainTitle().isEmpty()));
+    	browser.to(HomePageSpanish.class)
+    		.run(new HomePageSpanishToGermanAction())
+    		.run(new HomePageGermanToSpanishJavaAction());
     }
 }
